81d6cb4a277f60def4d785141ab880486e6bb084
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2019-10-03 16:59:31 +0200

    add uid

--- testuid.cxx
+++ testuid.cxx
@@ -1,4 +1,4 @@
-#include "vr.hxx"
+#include "uid.hxx"
 #include <cstdlib>
 #include <iostream>
 
@@ -9,6 +9,40 @@ using namespace dicm;
 
 TEST(uid_string, size)
 {
-  uid_t uid;
+  dicm::uid_t uid;
   EXPECT_EQ(64, sizeof uid);
 }
+
+TEST(uid_bcd, size)
+{
+  dicm::uid_bcd_t uid;
+  EXPECT_EQ(32, sizeof uid);
+}
+
+static const char * uids[] = {
+  "1234",
+  "1.2.3.4.5",
+  "1.3.6.1.4.35045.103501438824148998807202626810206788999",
+  "1.2.826.0.1.3680043.2.1143.5028470438645158236649541857909059554"
+};
+
+TEST(uid_string, print)
+{
+  for(const char * uid : uids)
+  {
+    std::cout << "value of uid: " << uid << std::endl;
+    dicm::uid_t uid_str = dicm::uid_t::make_from_string(uid);
+    std::cout << "value of uid_str: " << uid_str << std::endl;
+  }
+}
+
+TEST(uid_bcd, print)
+{
+  for(const char * uid : uids)
+  {
+    std::cout << "value of uid: " << uid << std::endl;
+    dicm::uid_bcd_t uid_bcd = dicm::uid_bcd_t::make_from_string(uid);
+    std::cout << "value of uid_str: " << uid_bcd << std::endl;
+  }
+}
+
--- uid.hxx
+++ uid.hxx
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "dicm_export.h"
-#include <cstdint>
 #include <iosfwd>
+#include <cstring>
 
 namespace dicm {
 namespace details {
@@ -18,7 +18,7 @@ struct DICM_EXPORT uid_string {
   static uid_string make_from_string(const char* in);
   friend bool operator==(const uid_string& lhs,
                          const uid_string& rhs) {
-    return memcmp(lhs.buf, rhs.buf, 64);
+    return std::strncmp(lhs.buf, rhs.buf, 64);
   }
 
 private:
@@ -30,6 +30,28 @@ private:
 
 DICM_EXPORT std::ostream & operator<<(std::ostream &os, const uid_string& uid);
 
+struct DICM_EXPORT uid_bcd { // packed BCD
+  /// Convert to an array:
+  typedef char str[64];
+  const str& to_array(str& in) const;
+
+  /// Construct from a pointer to array:
+  static uid_bcd make_from_string(const char* in);
+  friend bool operator==(const uid_bcd& lhs,
+                         const uid_bcd& rhs) {
+    return memcmp(lhs.buf, rhs.buf, 32);
+  }
+
+private:
+  friend std::ostream& operator<<(std::ostream& os,
+                                  const uid_bcd& uid);
+
+  unsigned char buf[32];
+};
+
+DICM_EXPORT std::ostream & operator<<(std::ostream &os, const uid_bcd& uid);
+
 }  // end namespace details
 typedef details::uid_string uid_t;
+typedef details::uid_bcd uid_bcd_t;
 }  // end namespace dicm
