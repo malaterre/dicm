c2baa1d87c604099ee6838a66987a10df7907ff9
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2020-09-09 09:02:45 +0200

    cleanup

--- java/dump.java
+++ java/dump.java
@@ -24,52 +24,56 @@ public class dump {
         XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
         try {
             XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(new FileInputStream(fileName));
-            while(xmlEventReader.hasNext()){
+            while(xmlEventReader.hasNext()) {
+                System.out.println("before" );
+ /*
+ *  Bulk reading occur in nextEvent(), not in hasdNext():
+javax.xml.stream.XMLStreamException: ParseError at [row,col]:[15,13]
+Message: Content is not allowed in trailing section.
+	at java.xml/com.sun.org.apache.xerces.internal.impl.XMLStreamReaderImpl.next(XMLStreamReaderImpl.java:652)
+	at java.xml/com.sun.xml.internal.stream.XMLEventReaderImpl.nextEvent(XMLEventReaderImpl.java:83)
+	at dump.parseXML(dump.java:29)
+	at dump.main(dump.java:20)
+ */
                 XMLEvent xmlEvent = xmlEventReader.nextEvent();
-//                System.out.println( xmlEvent.getEventType() );
+                System.out.println("after" );
 		switch( xmlEvent.getEventType() ) {
-//			case XMLStreamConstants.START_ELEMENT:
-//				System.out.println( "start" );
-//				break;
- case XMLStreamConstants.START_ELEMENT:
-System.out.println( "START_ELEMENT" );
-break;
- case XMLStreamConstants.END_ELEMENT:
-System.out.println( "END_ELEMENT" );
-break;
- case XMLStreamConstants.CHARACTERS:
-System.out.println( "CHARACTERS" );
-break;
- case XMLStreamConstants.ATTRIBUTE:
-System.out.println( "ATTRIBUTE" );
-break;
- case XMLStreamConstants.NAMESPACE:
-System.out.println( "NAMESPACE" );
-break;
- case XMLStreamConstants.PROCESSING_INSTRUCTION:
-System.out.println( "PROCESSING_INSTRUCTION" );
-break;
- case XMLStreamConstants.COMMENT:
-System.out.println( "COMMENT" );
-break;
- case XMLStreamConstants.START_DOCUMENT:
-System.out.println( "START_DOCUMENT" );
-break;
- case XMLStreamConstants.END_DOCUMENT:
-System.out.println( "END_DOCUMENT" );
-break;
- case XMLStreamConstants.DTD:
-System.out.println( "DTD" );
-break;
-default:
-System.out.println( "DEFAULT" );
+			case XMLStreamConstants.START_ELEMENT:
+				System.out.println( "START_ELEMENT" );
+				break;
+			case XMLStreamConstants.END_ELEMENT:
+				System.out.println( "END_ELEMENT" );
+				break;
+			case XMLStreamConstants.CHARACTERS:
+				System.out.println( "CHARACTERS" );
+				break;
+			case XMLStreamConstants.ATTRIBUTE:
+				System.out.println( "ATTRIBUTE" );
+				break;
+			case XMLStreamConstants.NAMESPACE:
+				System.out.println( "NAMESPACE" );
+				break;
+			case XMLStreamConstants.PROCESSING_INSTRUCTION:
+				System.out.println( "PROCESSING_INSTRUCTION" );
+				break;
+			case XMLStreamConstants.COMMENT:
+				System.out.println( "COMMENT" );
+				break;
+			case XMLStreamConstants.START_DOCUMENT:
+				System.out.println( "START_DOCUMENT" );
+				break;
+			case XMLStreamConstants.END_DOCUMENT:
+				System.out.println( "END_DOCUMENT" );
+				break;
+			case XMLStreamConstants.DTD:
+				System.out.println( "DTD" );
+				break;
+			default:
+				System.out.println( "DEFAULT" );
 		}
-// XMLStreamConstants.START_ELEMENT, XMLStreamConstants.END_ELEMENT, XMLStreamConstants.CHARACTERS, XMLStreamConstants.ATTRIBUTE, XMLStreamConstants.NAMESPACE, XMLStreamConstants.PROCESSING_INSTRUCTION, XMLStreamConstants.COMMENT, XMLStreamConstants.START_DOCUMENT, XMLStreamConstants.END_DOCUMENT, XMLStreamConstants.DTD
-            }
-            
+           }
         } catch (FileNotFoundException | XMLStreamException e) {
             e.printStackTrace();
         }
     }
-
 }
