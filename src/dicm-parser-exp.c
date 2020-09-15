3c0f227d80928406da71a641146dc51aa6b60308
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2020-09-15 17:21:05 +0200

    use stack

--- src/dicm-parser-exp.c
+++ src/dicm-parser-exp.c
@@ -39,7 +39,8 @@ int read_explicit(struct _src *src, struct _dataset *ds) {
     ide_t ide;      // implicit data element. 8 bytes
   } ude_t;
   assert(sizeof(ude_t) == 12);
-  struct _dataelement *de = &ds->de;
+  struct _dataelement *curde = &ds->de;
+  tag_t prevtag = curde->tag;
 
   if (ds->deflenitem == ds->curdeflenitem) {
     // End of Item
@@ -62,17 +63,17 @@ int read_explicit(struct _src *src, struct _dataset *ds) {
 #endif
 
   if (unlikely(is_tag_start(ude.ide.utag.tag))) {
-    de->tag = ude.ide.utag.tag;
-    de->vr = kINVALID;
-    de->vl = ude.ide.uvl.vl;
+    curde->tag = ude.ide.utag.tag;
+    curde->vr = kINVALID;
+    curde->vl = ude.ide.uvl.vl;
 
     if (sequenceoffragments >= 0) {
-      src->ops->seek(src, de->vl);
+      src->ops->seek(src, curde->vl);
       ds->sequenceoffragments++;
       return sequenceoffragments == 0 ? kBasicOffsetTable : kFragment;
-    } else if (de->vl != kUndefinedLength) {
+    } else if (curde->vl != kUndefinedLength) {
       assert(ds->deflenitem == kUndefinedLength);
-      assert(de->vl % 2 == 0);
+      assert(curde->vl % 2 == 0);
       ds->deflenitem = ude.ide.uvl.vl;
       if (ds->deflensq != kUndefinedLength) {
         // are we processing a defined length SQ ?
@@ -83,9 +84,9 @@ int read_explicit(struct _src *src, struct _dataset *ds) {
     return kItem;
   } else if (unlikely(is_tag_end_item(ude.ide.utag.tag)) ||
              unlikely(is_tag_end_sq(ude.ide.utag.tag))) {
-    de->tag = ude.ide.utag.tag;
-    de->vr = kINVALID;
-    de->vl = ude.ide.uvl.vl;
+    curde->tag = ude.ide.utag.tag;
+    curde->vr = kINVALID;
+    curde->vl = ude.ide.uvl.vl;
 
     if (unlikely(ude.ide.uvl.vl != 0)) return -kDicmReservedNotZero;
 
@@ -98,16 +99,16 @@ int read_explicit(struct _src *src, struct _dataset *ds) {
                                              : kSequenceOfItemsDelimitationItem;
   }
 
-  if (unlikely(!tag_is_lower(de, ude.ide.utag.tag))) {
+  if (unlikely(!tag_is_lower(curde, ude.ide.utag.tag))) {
     return -kDicmOutOfOrder;
   }
 
   // VR16 ?
-  vl_t vl;
+  dataelement_t de;
   if (is_vr16(ude.ede16.uvr.vr)) {
-    de->tag = ude.ede16.utag.tag;
-    de->vr = ude.ede16.uvr.vr;
-    vl = ude.ede16.uvl.vl16;
+    de.tag = ude.ede16.utag.tag;
+    de.vr = ude.ede16.uvr.vr;
+    de.vl = ude.ede16.uvl.vl16;
   } else {
     // padding must be set to zero
     if (unlikely(ude.ede32.uvr.vr.reserved != 0)) return -kDicmReservedNotZero;
@@ -115,18 +116,20 @@ int read_explicit(struct _src *src, struct _dataset *ds) {
     ret = src->ops->read(src, ude.ede32.uvl.bytes, 4);
     if (unlikely(ret < 4)) return -kNotEnoughData;
 
-    de->tag = ude.ede32.utag.tag;
-    de->vr = ude.ede32.uvr.vr.vr;
-    vl = ude.ede32.uvl.vl;
+    de.tag = ude.ede32.utag.tag;
+    de.vr = ude.ede32.uvr.vr.vr;
+    de.vl = ude.ede32.uvl.vl;
   }
 
-  if(vl != kUndefinedLength && vl % 2 != 0) return -kDicmOddDefinedLength;
-  de->vl = vl;
+  if(de.vl != kUndefinedLength && de.vl % 2 != 0) return -kDicmOddDefinedLength;
+  curde->tag = de.tag;
+  curde->vr = de.vr;
+  curde->vl = de.vl;
 
   if (tag_get_group(ude.ede32.utag.tag) == 0x2) {
-    assert(vl != kUndefinedLength);
-    assert(vl % 2 == 0);
-    src->ops->seek(src, de->vl);
+    assert(de.vl != kUndefinedLength);
+    assert(de.vl % 2 == 0);
+    src->ops->seek(src, curde->vl);
     return kFileMetaElement;
   } else if (is_tag_pixeldata(ude.ede32.utag.tag) && ude.ede32.uvr.vr.vr == kOB &&
              ude.ede32.uvl.vl == kUndefinedLength) {
@@ -136,22 +139,22 @@ int read_explicit(struct _src *src, struct _dataset *ds) {
   } else if (ude.ede32.uvr.vr.vr == kSQ && ude.ede32.uvl.vl == kUndefinedLength) {
     return kSequenceOfItems;
   } else if (ude.ede32.uvr.vr.vr == kSQ) {
-    assert(vl % 2 == 0);
+    assert(de.vl % 2 == 0);
     // defined length SQ
     assert(ds->deflensq == kUndefinedLength);
     ds->deflensq = ude.ede32.uvl.vl;
     return kSequenceOfItems;
   } else if (likely(tag_get_group(ude.ede32.utag.tag) >= 0x8)) {
-    assert(vl != kUndefinedLength);
-    assert(vl % 2 == 0);
-    src->ops->seek(src, de->vl);
+    assert(de.vl != kUndefinedLength);
+    assert(de.vl % 2 == 0);
+    src->ops->seek(src, curde->vl);
     if (ds->deflenitem != kUndefinedLength) {
       // are we processing a defined length Item ?
-      ds->curdeflenitem += compute_len(de);
+      ds->curdeflenitem += compute_len(curde);
     }
     if (ds->deflensq != kUndefinedLength) {
       // are we processing a defined length SQ ?
-      ds->curdeflensq += compute_len(de);
+      ds->curdeflensq += compute_len(curde);
     }
     return kDataElement;
   }
